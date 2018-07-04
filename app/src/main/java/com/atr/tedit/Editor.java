d0766a0a0d97ee947927a2b266e26a597f6b2cb2
Adam T. Ryder <atr167@yahoo.com>
2018-07-04 12:01:44 -0600

    Icon Editing
    Fine tuned the new icon with Android Image Studio.

--- app/src/main/java/com/atr/tedit/Editor.java
+++ app/src/main/java/com/atr/tedit/Editor.java
@@ -108,7 +108,8 @@ public class Editor extends Fragment {
 
             String mediaState = Environment.getExternalStorageState();
             if (!(Environment.MEDIA_MOUNTED.equals(mediaState)
-                    || Environment.MEDIA_MOUNTED_READ_ONLY.equals(mediaState))) {
+                    || Environment.MEDIA_MOUNTED_READ_ONLY.equals(mediaState))
+                    || !path.startsWith(Environment.getExternalStorageDirectory().getPath())) {
                 Toast.makeText(ctx, R.string.readonlymode, Toast.LENGTH_SHORT).show();
             }
         }
